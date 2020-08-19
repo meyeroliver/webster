e46a6a73a0fb129a77faa8dcdc3097f441a6049d
omeyer <meyeroliver9401@gmail.com>
2020-08-20 00:14:32 +0200

    created a knockout component using the template id, theres is still a lot of repeated htmlthat could be made to be a component. Dont like this way of doing it

--- js/form-component.js
+++ js/form-component.js
@@ -1,50 +1,9 @@
  define(['knockout', 'jquery'], function (ko, $) {
 
-     var userDetailsViewModel = function () {
-
-         this.firstname = ko.observable("Oliver")
-         this.lastname = ko.observable("Meyer")
-         this.email = ko.observable("oliver.meyer@netelek.co.za")
-
-     }
-
-     if (document.readyState === 'complete') {
-         ko.applyBindings(userDetailsViewModel);
-     }
- })
-
- /* define(['knockout', 'jquery'], function (ko, $) {
-
      ko.components.register('user-details-form', {
-         template: '<div class="row"> ' +
-             '<div class="col-6">' +
-             '<label for="first-name" class="label-form">First name:</label>' +
-             '</div>' +
-             '<div class="col-6">' +
-             '<input type="text" id="first-name" name="first-name"' +
-             'class="text-input-form text-input-border-form" data-bind="value:firstname">' +
-             '</div>' +
-             '</div>' +
-
-             '<div class="row">' +
-             '<div class="col-6">' +
-             '<label for="lastname" class="label-form">Lastname:</label>' +
-             '</div>' +
-             '<div class="col-6">' +
-             '<input type="text" id="lastname" name="lastname" class="text-input-form text-input-border-form"' +
-             'data-bind="value:lastname">' +
-             '</div>' +
-             '</div>' +
-
-             '<div class="row">' +
-             '<div class="col-6">' +
-             '<label for="email" class="label-form">Email:</label>' +
-             '</div>' +
-             '<div class="col-6">' +
-             '<input type="text" id="email" name="email" class="text-input-form text-input-border-form"' +
-             'data-bind="value:email">' +
-             '</div>' +
-             '</div>',
+         template: {
+             element: 'user-details-form-template'
+         },
          viewModel: function (params) {
              this.firstname = ko.observable(params.firstname)
              this.lastname = ko.observable(params.lastname)
@@ -56,7 +15,10 @@
      })
 
      if (document.readyState === 'complete') {
-         ko.applyBindings( /* userDetailsViewModel 
- );
- }
- }) */
+         ko.applyBindings();
+         /* var temp = document.getElementById('user-details-form-template')
+         var clone = temp.content.cloneNode(true)
+         document.getElementsByTagName("form")[0].appendChild(clone)
+ */
+     }
+ })
