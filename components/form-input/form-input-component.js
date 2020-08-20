567aa70c4785c4442ddeaa113cd725596823e24d
omeyer <meyeroliver9401@gmail.com>
2020-08-20 23:57:51 +0200

    updated the form component, at the moment it is not worth the time looking into making sexy individual form element component

--- components/form-input/form-input-component.js
+++ components/form-input/form-input-component.js
@@ -3,15 +3,7 @@
      var userDetailsViewModel = function (params) {
          this.firstname = ko.observable(params.firstname)
          this.lastname = ko.observable(params.lastname)
-         this.cellphone = ko.observable(params.cellphone)
          this.email = ko.observable(params.email)
-         this.address = ko.observable(params.address)
-         this.standNo = ko.observable(params.standNo)
-         this.accountNo = ko.observable(params.accountNo)
-         this.billingAddress1 = ko.observable(params.billingAddress1)
-         this.billingAddress2 = ko.observable(params.billingAddress2)
-         this.billingAddress3 = ko.observable(params.billingAddress3)
-         this.postalCode = ko.observable(params.postalCode)
          /* this.email = ko.pureComputed(function () {
              return this.firstname() + " " + this.lastname()
          }, this)
