8822d00bbef2372838100589f3c73905b18c8179
meyeroliver <meyeroliver9401@gmail.com>
2020-08-20 08:41:16 +0200

    extracted the viewModel and created an external version called userDetailsViewModel

--- js/form-component.js
+++ js/form-component.js
@@ -1,21 +1,22 @@
  define(['knockout', 'jquery'], function (ko, $) {
 
+     var userDetailsViewModel = function (params) {
+         this.firstname = ko.observable(params.firstname)
+         this.lastname = ko.observable(params.lastname)
+         this.email = ko.observable(params.email)
+
+         console.log(params)
+     }
+
      ko.components.register('user-details-form', {
          template: {
              element: 'user-details-form-template'
          },
-         viewModel: function (params) {
-             this.firstname = ko.observable(params.firstname)
-             this.lastname = ko.observable(params.lastname)
-             this.email = ko.observable(params.email)
-
-             console.log(params)
-
-         }
+         viewModel: userDetailsViewModel
      })
 
      if (document.readyState === 'complete') {
-         ko.applyBindings();
+         ko.applyBindings(userDetailsViewModel);
          /* var temp = document.getElementById('user-details-form-template')
          var clone = temp.content.cloneNode(true)
          document.getElementsByTagName("form")[0].appendChild(clone)
