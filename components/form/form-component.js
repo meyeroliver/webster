0976a6fb3ff8c311d9d592e5e943b199bcc0b809
meyeroliver <meyeroliver9401@gmail.com>
2020-08-21 10:26:50 +0200

    using the observable value of the checkbox to determine the visibility of the tenantComponent using ko.computed

--- components/form/form-component.js
+++ components/form/form-component.js
@@ -12,10 +12,26 @@
          this.billingAddress2 = ko.observable(params.billingAddress2)
          this.billingAddress3 = ko.observable(params.billingAddress3)
          this.postalCode = ko.observable(params.postalCode)
-         /* this.email = ko.pureComputed(function () {
-             return this.firstname() + " " + this.lastname()
+         this.tenantName = ko.observable(params.tenantName)
+         this.tenantSurname = ko.observable(params.tenantSurname)
+         this.tenantCellphone = ko.observable(params.tenantCellphone)
+         this.tenantEmail = ko.observable(params.tenantEmail)
+
+
+         this.hasTenant = ko.observable()
+         /**
+          * TODO
+          * -> Check a bit deeper into subscribers
+          */
+         /* this.hasTenant.subscribe(function (hasTentantValue) {
+             if (hasTentantValue) {
+                 alert("awe awe awe")
+                 self.showTenantContainer(true)
+             }
+         }) */
+         this.showTenantContainer = ko.computed(function () {
+             return this.hasTenant()
          }, this)
- */
      }
      return {
          viewModel: userDetailsViewModel,
