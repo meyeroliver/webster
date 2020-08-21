c95a5c9ed9d037815a1a8ddb2312706fbfd5d62e
omeyer <meyeroliver9401@gmail.com>
2020-08-21 22:11:09 +0200

    ...

--- components/form/form-component.js
+++ components/form/form-component.js
@@ -1,40 +1,43 @@
- define(['knockout', 'text!/components/form/form-template.html'], function (ko, formTemplate) {
+define(['knockout', 'text!/components/form/form-template.html'], function (ko, formTemplate) {
 
-     var userDetailsViewModel = function (params) {
-         this.firstname = ko.observable(params.firstname)
-         this.lastname = ko.observable(params.lastname)
-         this.cellphone = ko.observable(params.cellphone)
-         this.email = ko.observable(params.email)
-         this.address = ko.observable(params.address)
-         this.standNo = ko.observable(params.standNo)
-         this.accountNo = ko.observable(params.accountNo)
-         this.billingAddress1 = ko.observable(params.billingAddress1)
-         this.billingAddress2 = ko.observable(params.billingAddress2)
-         this.billingAddress3 = ko.observable(params.billingAddress3)
-         this.postalCode = ko.observable(params.postalCode)
-         this.tenantName = ko.observable(params.tenantName)
-         this.tenantSurname = ko.observable(params.tenantSurname)
-         this.tenantCellphone = ko.observable(params.tenantCellphone)
-         this.tenantEmail = ko.observable(params.tenantEmail)
+    
+
+    var userDetailsViewModel = function (params) {
+        this.firstname = ko.observable(params.firstname)
+        this.lastname = ko.observable(params.lastname)
+        this.cellphone = ko.observable(params.cellphone)
+        this.email = ko.observable(params.email)
+        this.address = ko.observable(params.address)
+        this.standNo = ko.observable(params.standNo)
+        this.accountNo = ko.observable(params.accountNo)
+        this.billingAddress1 = ko.observable(params.billingAddress1)
+        this.billingAddress2 = ko.observable(params.billingAddress2)
+        this.billingAddress3 = ko.observable(params.billingAddress3)
+        this.postalCode = ko.observable(params.postalCode)
+        this.tenantName = ko.observable(params.tenantName)
+        this.tenantSurname = ko.observable(params.tenantSurname)
+        this.tenantCellphone = ko.observable(params.tenantCellphone)
+        this.tenantEmail = ko.observable(params.tenantEmail)
 
 
-         this.hasTenant = ko.observable()
-         /**
-          * TODO
-          * -> Check a bit deeper into subscribers
-          */
-         /* this.hasTenant.subscribe(function (hasTentantValue) {
-             if (hasTentantValue) {
-                 alert("awe awe awe")
-                 self.showTenantContainer(true)
-             }
-         }) */
-         this.showTenantContainer = ko.computed(function () {
-             return this.hasTenant()
-         }, this)
-     }
-     return {
-         viewModel: userDetailsViewModel,
-         template: formTemplate
-     }
- })
+        this.hasTenant = ko.observable()
+        /**
+         * TODO
+         * -> Check a bit deeper into subscribers
+         */
+        /* this.hasTenant.subscribe(function (hasTentantValue) {
+            if (hasTentantValue) {
+                alert("awe awe awe")
+                self.showTenantContainer(true)
+            }
+        }) */
+        this.showTenantContainer = ko.computed(function () {
+
+            return this.hasTenant()
+        }, this)
+    }
+    return {
+        viewModel: userDetailsViewModel,
+        template: formTemplate
+    }
+})
