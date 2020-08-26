f3be5b42b726f10bc10b2dbfe3ad64abb266bda5
meyeroliver <meyeroliver9401@gmail.com>
2020-08-26 10:56:15 +0200

    meter saving and updating the customer object

--- components/meter/meter-view-model.js
+++ components/meter/meter-view-model.js
@@ -1,62 +1,61 @@
-define(['knockout', 'text!/components/meter/meter-template.html', 'customerDoa'],
-    function (ko, meterTemplate, customerDoa) {
+define([
+  "knockout",
+  "text!/components/meter/meter-template.html",
+  "customerDoa",
+], function (ko, meterTemplate, customerDoa) {
+  var meterViewModel = function (params) {
+    var self = this;
+    self.takeImage = ko.observable(true);
+    self.meterType = ko.observable(params.meterType);
+    self.consumptionType = ko.observable(params.consumptionType);
+    self.serialNo = ko.observable(params.serialNo);
 
-        var meterViewModel = function (params) {
-            var self = this
-            self.takeImage = ko.observable(true)
-            self.meterType = ko.observable(params.meterType)
-            self.consumptionType = ko.observable(params.consumptionType)
-            self.serialNo = ko.observable(params.serialNo)
+    const inpFile = document.getElementById("meter-image");
+    const imgContainer = document.getElementById("imagePreview");
+    const capturedImg = imgContainer.querySelector(".captured-image");
 
-            const inpFile = document.getElementById("meter-image")
-            const imgContainer = document.getElementById("imagePreview")
-            const capturedImg = imgContainer.querySelector(".captured-image")
+    var hideImgIcon = self.takeImage();
 
-            var hideImgIcon = self.takeImage()
+    inpFile.addEventListener("change", function () {
+      const file = this.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        /**
+         *waits for image to finish loading
+         */
+        reader.addEventListener("load", function () {
+          /**
+           *This sets the captured image to the element
+           */
+          self.takeImage(false);
+          capturedImg.setAttribute("src", this.result);
+          /**
+           * TODO
+           * - convert image to blob and save into index db under the current customer
+           * - allow for persistence across reloads
+           */
+        });
+      }
+      console.log(file);
+    });
 
-            inpFile.addEventListener("change", function () {
-                const file = this.files[0]
-                if (file) {
-                    const reader = new FileReader();
-                    reader.readAsDataURL(file)
-                    /**
-                     *waits for image to finish loading
-                     */
-                    reader.addEventListener("load", function () {
-                        /**
-                         *This sets the captured image to the element
-                         */
-                        self.takeImage(false)
-                        capturedImg.setAttribute("src", this.result)
-                        /**
-                         * TODO
-                         * - convert image to blob and save into index db under the current customer
-                         * - allow for persistence across reloads
-                         */
-                    })
-                }
-                console.log(file)
-            })
+    saveMeter = function () {
+      let mockMeter = {
+        meterType: self.meterType(),
+        consumptionType: self.consumptionType(),
+        serialNo: self.serialNo(),
+      };
 
-            saveMeter = function () {
+      customerDoa.updateByCustomerId(mockMeter, 1);
+    };
 
-                let mockMeter = {
-                    meterType: self.meterType(),
-                    consumptionType: self.consumptionType(),
-                    serialNo: self.serialNo()
-
-                };
-
-                customerDoa.updateByCustomerId(mockMeter, 2)
-            }
-
-            removeMeter = function () {
-                alert('awe, Remove Meter was clicked')
-            }
-
-        }
-        return {
-            viewModel: meterViewModel,
-            template: meterTemplate
-        }
-    })
+    removeMeter = function () {
+      alert("awe, Remove Meter was clicked");
+    };
+  };
+  return {
+    viewModel: meterViewModel,
+    template: meterTemplate,
+  };
+});
--- js/db/doas/customer-doa.js
+++ js/db/doas/customer-doa.js
@@ -1,111 +1,117 @@
-define(['knockout', 'captureDB'], function (ko, dbConn) {
+define(["knockout", "captureDB"], function (ko, dbConn) {
+  //TODO: return something that can be used as a reference for later
+  //TODO: convert to promise base
+  var custumerDoa = {
+    insert: function (data) {
+      let db = dbConn.result;
+      var transaction = db.transaction("customers", "readwrite");
+      var customersStore = transaction.objectStore("customers");
 
-    //TODO: return something that can be used as a reference for later
-    //TODO: convert to promise base
-    var custumerDoa = {
-        insert: function (data) {
-            let db = dbConn.result
-            var transaction = db.transaction('customers', 'readwrite')
-            var customersStore = transaction.objectStore('customers')
+      let transRequest = customersStore.add(data); // (3)
 
-            let transRequest = customersStore.add(data); // (3)
+      transRequest.onsuccess = function () {
+        // (4)
+        console.log(
+          "A customer was added to the objectStore [Table]",
+          transRequest
+        );
+      };
 
-            transRequest.onsuccess = function () { // (4)
-                console.log("A customer was added to the objectStore [Table]", transRequest);
-            };
+      transRequest.onerror = function () {
+        console.log("Error", transRequest.error.message);
+      };
 
-            transRequest.onerror = function () {
-                console.log("Error", transRequest.error.message);
-            };
+      transaction.oncomplete = function () {
+        console.log("closing connection to database");
+        //db.close();
+      };
+    },
+    updateByCustomerId: function (meter, cId) {
+      let db = dbConn.result;
+      var transaction = db.transaction("customers", "readwrite");
+      var customersStore = transaction.objectStore("customers");
+      console.log(meter);
+      let getRequest = customersStore.get(cId); // (3)
 
-            transaction.oncomplete = function () {
-                console.log("closing connection to database")
-                //db.close();
-            }
-        },
-        updateByCustomerId: function (meter, cId) {
-            let db = dbConn.result
-            var transaction = db.transaction('customers', 'readwrite')
-            var customersStore = transaction.objectStore('customers')
+      getRequest.onsuccess = function () {
+        // (4)
+        var customer = getRequest.result;
+        console.log(customer);
+        customer.meterType = meter.meterType;
+        customer.consumptionType = meter.consumptionType;
+        customer.serialNo = meter.serialNo;
 
-            let getRequest = customersStore.get(cId); // (3)
+        let updateRequest = customersStore.put(customer, cId);
 
-            getRequest.onsuccess = function () { // (4)
-                var customer = getRequest.result;
-                console.log(customer)
-                customer.meterType = meter.meterType
-                customer.consumptionType = meter.consumptionType
-                customer.serialNo = meter.serialNo
+        updateRequest.onsuccess = function () {
+          console.log(updateRequest.result);
+          console.log(
+            "A customer was update to the objectStore [Table]",
+            updateRequest
+          );
+        };
+        updateRequest.onerror = function () {
+          console.log("Error", updateRequest.error.message);
+        };
+      };
 
-                let updateRequest = customersStore.put(customer, cId);
+      getRequest.onerror = function () {
+        console.log("Error", getRequest.error.message);
+      };
 
-                updateRequest.onsuccess = function () {
-                    console.log(updateRequest.result)
-                    console.log("A customer was update to the objectStore [Table]", updateRequest);
+      transaction.oncomplete = function () {
+        console.log("closing connection to database");
+        //db.close();
+      };
+    },
+    getCustomerById: function (cID) {
+      let db = dbConn.result;
 
-                }
-                updateRequest.onerror = function () {
-                    console.log("Error", updateRequest.error.message);
-                };
+      var transaction = db.transaction("customers", "readonly");
+      var customersStore = transaction.objectStore("customers");
 
-            };
+      let transRequest = customersStore.get(cID); // (3)
 
-            getRequest.onerror = function () {
-                console.log("Error", getRequest.error.message);
-            };
+      transRequest.onsuccess = function () {
+        // (4)
+        console.log(transRequest.result);
+      };
 
-            transaction.oncomplete = function () {
-                console.log("closing connection to database")
-                //db.close();
-            }
+      transRequest.onerror = function () {
+        console.log("Error", transRequest.error.message);
+      };
 
-        },
-        getCustomerById: function (cID) {
-            let db = dbConn.result;
+      transaction.oncomplete = function () {
+        console.log("closing connection to database");
+        //db.close();
+      };
+    },
+    getAllCustomers: function () {
+      let db = dbConn.result;
 
-            var transaction = db.transaction('customers', 'readonly')
-            var customersStore = transaction.objectStore('customers')
+      var transaction = db.transaction("customers", "readonly");
+      var customersStore = transaction.objectStore("customers");
+      //todo : use queries on the get all, eg ready-for-upload
+      let transRequest = customersStore.getAll(); // (3)
 
-            let transRequest = customersStore.get(cID); // (3)
+      transRequest.onsuccess = function () {
+        // (4)
+        console.log(transRequest.result);
+      };
 
-            transRequest.onsuccess = function () { // (4)
-                console.log(transRequest.result);
-            };
+      transRequest.onerror = function () {
+        console.log("Error", transRequest.error.message);
+      };
 
-            transRequest.onerror = function () {
-                console.log("Error", transRequest.error.message);
-            };
+      transaction.oncomplete = function () {
+        console.log("closing connection to database");
+        //db.close();
+      };
+    },
+    deleteCustomerById: function (cID) {
+      console.log("this will be use to delete the customer");
+    },
+  };
 
-            transaction.oncomplete = function () {
-                console.log("closing connection to database")
-                //db.close();
-            }
-        },
-        getAllCustomers: function () {
-            let db = dbConn.result;
-
-            var transaction = db.transaction('customers', 'readonly')
-            var customersStore = transaction.objectStore('customers')
-            //todo : use queries on the get all, eg ready-for-upload
-            let transRequest = customersStore.getAll(); // (3)
-
-            transRequest.onsuccess = function () { // (4)
-                console.log(transRequest.result);
-            };
-
-            transRequest.onerror = function () {
-                console.log("Error", transRequest.error.message);
-            };
-
-            transaction.oncomplete = function () {
-                console.log("closing connection to database")
-                //db.close();
-            }
-        },
-        deleteCustomerById: function (cID) {
-            console.log('this will be use to delete the customer');
-        }
-    }
-
-    return custumerDoa;
-})
+  return custumerDoa;
+});
