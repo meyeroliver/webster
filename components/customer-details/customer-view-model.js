1e0375e674102a5aad6e1cc3a14d96e0eb20038d
meyeroliver <meyeroliver9401@gmail.com>
2020-08-27 09:58:01 +0200

    added knockout to meter for confirm-details

--- components/customer-details/customer-view-model.js
+++ components/customer-details/customer-view-model.js
@@ -5,27 +5,35 @@ define([
 ], function (ko, customerTemplate, customerDoa) {
     var customerViewModel = function (params) {
         var self = this;
-        self.customerName = ko.observable()
-        self.customerCellPhone = ko.observable()
-        self.customerEmail = ko.observable()
+        self.customerName = ko.observable();
+        self.customerCellPhone = ko.observable();
+        self.customerEmail = ko.observable();
+
+        self.meterType = ko.observable();
+        self.consumptionType = ko.observable();
+        self.serialNo = ko.observable();
 
         async function testicle() {
-            return await customerDoa.getCustomerById(1)
-
+            return await customerDoa.getCustomerById(1);
         }
-        testicle().then((customer) => {
-            self.customerName(customer.customerName)
-            self.customerCellPhone(customer.customerCellPhone)
-            self.customerEmail(customer.customerEmail)
-            console.log(customer)
-        }).catch((err) => {
-            console.log(err)
-        })
+        testicle()
+            .then((customer) => {
+                self.customerName(customer.customerName);
+                self.customerCellPhone(customer.customerCellphone);
+                self.customerEmail(customer.customerEmail);
+                self.meterType(customer.meterType);
+                self.consumptionType(customer.consumptionType);
+                self.serialNo(customer.serialNo);
 
+                console.log(customer);
 
-    }
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
     return {
         viewModel: customerViewModel,
-        template: customerTemplate
-    }
-})
+        template: customerTemplate,
+    };
+});
